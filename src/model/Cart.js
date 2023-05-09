b3a6a4d9b8d2f1de269b1608191503473f9e5055
HA-1-N <hoanganh02062001@gmail.com>
2023-05-09 11:05:57 +0700

    feat(cart): model

--- src/model/Cart.js
+++ src/model/Cart.js
@@ -1,10 +1,42 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const CartSchema = new Schema({
-  userId: {
-    type: String,
-    required: true,
+const CartSchema = new Schema(
+  {
+    orderBy: {
+      type: String,
+      required: true,
+      ref: "User",
+    },
+    products: [
+      {
+        product: {
+          type: String,
+          ref: "Product",
+        },
+        count: Number,
+        color: String,
+        price: Number,
+      },
+    ],
+    cartTotal: Number,
+    totalAfterDiscount: Number,
   },
-  products: [],
+  { timestamps: true }
+);
+
+Cart.virtual("user", {
+  ref: "User",
+  localField: "orderBy",
+  foreignField: "userName",
+  justOne: true,
 });
+
+Cart.virtual("product", {
+  ref: "Product",
+  localField: "product",
+  foreignField: "productCode",
+  justOne: true,
+});
+
+module.exports = mongoose.model("Cart", CartSchema);
